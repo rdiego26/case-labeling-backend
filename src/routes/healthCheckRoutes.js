5fbe6b694e2c3a9287aa1d1387593df61fefa81b
rdiego26 <rdieg26@gmail.com>
2021-12-08 20:21:23 +0000

    feature: add docker and docker-compose.

--- src/routes/healthCheckRoutes.js
+++ src/routes/healthCheckRoutes.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
   const controller = require('../controllers/healthCheckController');
   const router = require('express').Router();
 
-  router.get('/api/healthCheck', controller.checkHealth);
+  router.get('/healthCheck', controller.checkHealth);
 
   app.use(router);
 };
