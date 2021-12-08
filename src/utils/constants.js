2c041601341dc3da4f11e81f8cc47da3eb57129e
rdiego26 <rdieg26@gmail.com>
2021-12-08 20:50:55 +0000

    feature: ADD mongo and mongoose

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,10 @@ const appData = require('../../package.json');
 
 const constants = {
 
+  database: {
+    uri: process.env.DATABASE_URI || 'mongodb://localhost:27017/erh',
+  },
+
   app: {
     name: appData.name,
     limitItemsPerPage: process.env.LIMIT_ITEMS_PER_PAGE || 30,
